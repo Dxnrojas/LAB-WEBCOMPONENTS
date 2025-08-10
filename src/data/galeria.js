59777ee7e1dac1de779bf36c6d6ec172ce06a714
Daniela Rojas <drojasaristi19@gmail.com>
2025-08-10 17:08:53 -0500

    chore: add initial project structure

---
+++ src/data/galeria.js
@@ -0,0 +1,49 @@
+export const galeria = [
+  {
+    id: 1,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/127189/330d48eecf496b8c5b30c4d739dccc12_722294072503585298.jpg",
+    title: "Honkai Impact 3D",
+    description: "Honkai Impact 3rd Launching v7.9 [Stars Derailed] on",
+  },
+  {
+    id: 2,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/127049/f6dfee9a74478eccbe59c547277241df_9046600959592478924.jpeg?x-oss-process=image/resize,w_800/quality,q_80",
+    title: "Genshin Impact",
+    description:
+      "Genshin Impact Version 5.2: Enhanced Aerial Abilities in Natlan – Coming November 20",
+  },
+  {
+    id: 3,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/158030/b9d176dc671c89701961dc9ebf8a13d1_310738835300878350.jpeg?x-oss-process=image/resize,w_800/quality,q_80",
+    title: "Genshin Impact",
+    description:
+      "Genshin Impact Version 5.8 Refreshes Your Summer at Easybreeze Holiday Resort in Natlan on July 30!",
+  },
+  {
+    id: 4,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/157510/be36c01624f6f4885b462cb00b642720_7636524794181138012.jpeg?x-oss-process=image/resize,w_800/quality,q_80",
+    title: "Genshin Impact",
+    description:
+      "Zenless Zone Zero Version 2.1 \"The Impending Crash of Waves\" Sets Sail for Summer Paradise on July 16",
+  },
+  {
+    id: 5,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/157512/86fbed53ed84c2fd973b8b6b30dde0a7_3437804973122476957.jpeg?x-oss-process=image/resize,w_800/quality,q_80",
+    title: "HoYoverse",
+    description: "HoYoverse Announces Gamescom 2025 Lineup",
+  },
+  {
+    id: 6,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/157426/b9e29e1a0be0f5c5b37f5766cb084033_6477336873377254210.jpeg?x-oss-process=image/resize,w_800/quality,q_80",
+    title: "Honkai: Star Rail",
+    description:
+      "Honkai: Star Rail Version 3.4 “For the Sun is Set to Die” Will Land on July 02",
+  },
+  {
+    id: 7,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/157079/28f78d5993bbc28bc37ee9d59b92be59_6590477694943496865.jpeg?x-oss-process=image/resize,w_800/quality,q_80",
+    title: "Genshin Impact",
+    description:
+      "Genshin Impact Version 5.7 Welcomes Skirk, the Sinner's Pupil and Fool's Master, on June 18th",
+  },
+];
---
+++ src/data/products.js
@@ -0,0 +1,45 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://webstatic.hoyoverse.com/upload/op-public/2022/02/08/eb228c7d178a684934d3cbb9189e5fb0_8364089112069254680.jpeg?x-oss-process=image/resize,w_750/quality,q_80",
+    title: "Honkai: Star Rail",
+    description: "May this journey lead us starward",
+    button: "more",
+  },
+  {
+    id: 2,
+    img: "https://webstatic.hoyoverse.com/upload/op-public/2023/03/29/a0ea4e26881dd50ac78a2cd16287dea9_6483003708536667540.jpeg?x-oss-process=image/resize,w_750/quality,q_80",
+    title: "Honkai Impact 3rd",
+    description: "Fight for all that is beautiful in the world",
+    button: "more",
+  },
+  {
+    id: 3,
+    img: "https://webstatic.hoyoverse.com/upload/op-public/2023/03/29/a0ea4e26881dd50ac78a2cd16287dea9_6483003708536667540.jpeg?x-oss-process=image/resize,w_750/quality,q_80",
+    title: "Genshin Impact",
+    description: "Step into a vast magical world of adventure",
+    button: "more",
+  },
+  {
+    id: 4,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/102135/0b48b720b0628f790f597b1eca1cff9a_1294670149771623204.jpg?x-oss-process=image/resize,w_750/quality,q_80",
+    title: "Zenless Zone Zero",
+    description: "Welcome to New Eridu!",
+    button: "more",
+  },
+  {
+    id: 5,
+    img: "https://fastcdn.hoyoverse.com/content-v2/plat/101581/33c30722b5c67a6b40224a34bd556b62_6748671822616201056.jpeg?x-oss-process=image/resize,w_750/quality,q_80",
+    title: "HoYoLAB",
+    description:
+      "HoYoLAB is a community forum, featuring official information on our popular games, a variety of fan art, useful tools, exclusive perks, player anecdotes, and more.",
+    button: "more",
+  },
+  {
+    id: 6,
+    img: "https://webstatic.hoyoverse.com/upload/op-public/2022/02/07/ca6a1f4b2b8208b29b408740968ce647_4500883843699713766.jpg?x-oss-process=image/resize,w_750/quality,q_80",
+    title: "Tears of Themis",
+    description: "Embark now on an adventure of romance and wits!",
+    button: "more",
+  },
+];
